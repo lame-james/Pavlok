0b44fdb613c092988482b8defaa60c3290c03a28
James Hall <67132041+lame-james@users.noreply.github.com>
2022-11-28 12:30:58 +0800

    Update

--- pythonTest/test.py
+++ pythonTest/test.py
@@ -1,5 +1,36 @@
+import curses
 from PyPav2 import Pavlok
 
-device = Pavlok(mac="e1:8d:53:fe:e6:3e")
 
-device.beep(15)
+# device = Pavlok(mac="E1:12:63:5D:60:A0")
+device2 = Pavlok(mac="F7:42:5B:0B:80:25")
+# device3 = Pavlok(mac="E1:8D:53:FE:E6:3E")
+choice = 4
+
+
+print(device2.read("0x0013"))
+
+
+
+while(choice != 0) :
+  print("Vibrate - 1")
+  print("Beep - 2")
+  print("Shock - 3")
+  print("Quit - 0")
+  choice = int(input("Choice: "))
+
+
+  if(choice == 1) :
+    #device.vibrate(7)
+    device2.vibrate(7)
+    #device3.vibrate(7)
+  elif(choice == 2) :
+    #device.beep(7)
+    device2.beep(7)
+    #device3.beep(7)
+  elif(choice == 3) :
+    #device.shock(7)
+    device2.shock(7)
+    #device3.shock(7)
+
+
